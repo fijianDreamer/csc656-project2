3a687a1883f4313f633a4c218c9cc2e6b7400217
fijianDreamer <amdesingh92@gmail.com>
2016-04-16 21:07:41 -0700

    Added files via upload

---
+++ csc656Proj2Sys1.cpp
@@ -0,0 +1,16 @@
+// Amrita Singh
+// csc 656 Project 2 - Cache Simulation System 1
+// Due: Monday, April 25, 2016 
+// csc656Proj2Sys1.cpp
+/* The purpose of this projet is to write a cache simulator that uses memory
+*  address traces as input.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Everything works yay!!" << endl;
+	return 0;
+}
